494999cb298c02ee8fd2e06d0b93be7a1ffab7e9
Slim <slimt@ebos.com.cy>
2024-11-07 15:08:18 +0100

    Add expose headers option

--- server.js
+++ server.js
@@ -23,9 +23,10 @@ const app = express();
 
 // Allow requests from your frontend origin
 app.use(cors({
-  origin: 'http://localhost:3000',  // Make sure this is your frontend's URL
+  origin: 'http://localhost:3000/',  // Make sure this is your frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed methods
-  credentials: true  // Allow credentials if needed (e.g., for cookies)
+  credentials: true,  // Allow credentials if needed (e.g., for cookies)
+  exposedHeaders:"access-control-allow-origin,access-control-allow-methods,access-control-allow-headers"
 }));
 
 // Middleware to parse JSON and URL-encoded bodies
